0b2dcc8f970bf1d3e370d30e496c09371fa6a456
Emmanuel-code <b.emma.j37@outlook.com>
2020-01-20 02:23:35 -1200

    readme

--- mySite2/settings.py
+++ mySite2/settings.py
@@ -17,8 +17,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 EMAIL_HOST= 'smtp.gmail.com'
-EMAIL_HOST_USER	= 'b.emma.j37@gmail.com'
-EMAIL_HOST_PASSWORD	= 'BejEtube'
+EMAIL_HOST_USER	= ''
+EMAIL_HOST_PASSWORD	= ''
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 ACCOUNT_EMAIL_VERIFICATION='none'
