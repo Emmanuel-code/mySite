04b7513369eff8774139ffe192bd8a69361aa738
Emmanuel-code <b.emma.j37@outlook.com>
2020-01-24 22:16:47 -1200

    another commit

--- mySite2/settings.py
+++ mySite2/settings.py
@@ -173,7 +173,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/blog/static/'
+STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'blog:post_list'
 LOGIN_URL = 'login'
